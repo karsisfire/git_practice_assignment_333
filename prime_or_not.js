4e527adae07b265b732cc887320a2e72a3dce554
karsisfire <karstech620338@gmail.com>
2023-09-04 22:20:32 +0530

    Prime or Not

---
+++ prime_or_not.js
@@ -0,0 +1,13 @@
+let num = 13;
+let facto = 0;
+for(let i = 0; i <= num; i++){
+    if(num % i == 0){
+        facto += 1;
+    }
+}
+if(facto < 2){
+    console.log("Prime");
+}
+else{
+    console.log("Not_Prime");
+}
